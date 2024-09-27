b734176853f57c862703077623cfdf075420db05
I Putu Angga Prawira Prathama <120653568+anggaprxwira@users.noreply.github.com>
2024-09-27 11:19:37 +0700

    crud.php

--- form-cookies/crud.php
+++ form-cookies/crud.php
@@ -1,18 +1,22 @@
-<?php 
-    include 'koneksi.php';
+<?php
+include 'koneksi.php';
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $discord_id = $_POST['discord_id'];
-        $price = $_POST['price'];
-
-        $sql = "INSERT INTO pesan (username, password, discord_id, price) VALUES ('$username', '$password', '$discord_id', '$price')";
-
-        if ($connect->query($sql) === TRUE) {
-            echo "Data Berhasil Diinsert!";
-        } else {
-            echo "Error: " . $sql . "<br>" . $connect->error;
-        }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_COOKIE['discordFormData'])) {
     }
-?>
+    $formData = json_decode($_COOKIE['discordFormData'], true);
+    $username = $formData['username'];
+    $password = $formData['password'];
+    $discord_id = $formData['discord_id'];
+    $price = $formData['price'];
+
+    $sql = "INSERT INTO pesan (username, password, discord_id, price) VALUES ('$username', '$password', '$discord_id', '$price')";
+
+    if ($connect->query($sql) === TRUE) {
+        echo "Data Berhasil Diinsert!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $connect->error;
+    }
+} else {
+    echo "Cookie not set!";
+}
